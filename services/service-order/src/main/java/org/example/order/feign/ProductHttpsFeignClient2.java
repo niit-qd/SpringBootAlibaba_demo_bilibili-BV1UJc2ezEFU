bfa964b66e59ef26a2c33af83497c472603a5e9a
niit-qd <niit-qd@users.noreply.github.com>
2025-03-31 02:48:36 +0800

    Update ProductHttpsFeignClient2.java

--- services/service-order/src/main/java/org/example/order/feign/ProductHttpsFeignClient2.java
+++ services/service-order/src/main/java/org/example/order/feign/ProductHttpsFeignClient2.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// @FeignClient(name = "service-product-https", path = "/product")
-@FeignClient(name = "https://service-product-https", path = "/product")
+@FeignClient(name = "service-product-https", path = "/product")
+// @FeignClient(name = "https://service-product-https", path = "/product")
 public interface ProductHttpsFeignClient2 {
 
 
